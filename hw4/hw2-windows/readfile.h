ec81b9fdcb35e2e27e35bbaee11936c65d0d1a3b
calvinwoo <calvinwoo@berkeley.edu>
2012-12-01 19:16:14 -0800

    calvin test

--- hw4/hw2-windows/readfile.h
+++ hw4/hw2-windows/readfile.h
@@ -1,6 +1,5 @@
 // Readfile definitions 
 
-//kjhh
 void matransform (stack<mat4> &transfstack, GLfloat * values) ;
 void rightmultiply (const mat4 & M, stack<mat4> &transfstack) ;
 bool readvals (stringstream &s, const int numvals, GLfloat * values) ;
