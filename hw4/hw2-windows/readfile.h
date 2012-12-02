eb624d4b2ab122eff0a29f1549b09122e13d199a
chris_wu <chriswu452@gmail.com>
2012-12-01 19:16:56 -0800

    modified readfile.h

--- hw4/hw2-windows/readfile.h
+++ hw4/hw2-windows/readfile.h
@@ -5,3 +5,5 @@ void matransform (stack<mat4> &transfstack, GLfloat * values) ;
 void rightmultiply (const mat4 & M, stack<mat4> &transfstack) ;
 bool readvals (stringstream &s, const int numvals, GLfloat * values) ;
 void readfile (const char * filename) ;
+
+//hjbj
